97ea1d71709bc73b03acd0900118e02cbaaba4e9
jucelio farias coelho <82785673+juceliocoelho@users.noreply.github.com>
2022-02-19 11:22:16 -0300

    criacao_projeto

---
+++ primeiro-programa-java/src/java_executavel/primeiraClasseJava.java
@@ -0,0 +1,12 @@
+package java_executavel;
+
+public class primeiraClasseJava {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Meu primeiro programaem java ");
+	
+	}
+	
+}
+ 
